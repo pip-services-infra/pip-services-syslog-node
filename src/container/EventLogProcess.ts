3794025da0439f23d3e215382d786ffaa6865878
Dmitriy Krayniy <krdima92@gmail.com>
2018-04-10 15:46:35 +0300

    added pip-services-oss

--- src/container/EventLogProcess.ts
+++ src/container/EventLogProcess.ts
@@ -1,5 +1,6 @@
 import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
+import { DefaultOssFactory } from 'pip-services-oss-node';
 
 import { EventLogServiceFactory } from '../build/EventLogServiceFactory';
 
@@ -8,6 +9,7 @@ export class EventLogProcess extends ProcessContainer {
     public constructor() {
         super("eventlog", "System event logging microservice");
         this._factories.add(new EventLogServiceFactory);
+        this._factories.add(new DefaultOssFactory);
     }
 
 }
