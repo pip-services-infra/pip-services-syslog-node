08bc95958bb702507f0da3aabb9c438d6a932d2f
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 13:22:34 +0300

    added DefaultRpcFactory

--- obj/src/container/EventLogProcess.js
+++ obj/src/container/EventLogProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const EventLogServiceFactory_1 = require("../build/EventLogServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class EventLogProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("eventlog", "System event logging microservice");
         this._factories.add(new EventLogServiceFactory_1.EventLogServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.EventLogProcess = EventLogProcess;
--- src/container/EventLogProcess.ts
+++ src/container/EventLogProcess.ts
@@ -2,12 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { EventLogServiceFactory } from '../build/EventLogServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class EventLogProcess extends ProcessContainer {
 
     public constructor() {
         super("eventlog", "System event logging microservice");
         this._factories.add(new EventLogServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 
 }
